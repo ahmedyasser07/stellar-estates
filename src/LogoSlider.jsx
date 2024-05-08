ac757c7093ddbe34750c09444be71a33fae52693
YoussefAhmed108 <youssef.ahmed108@gmail.com>
2024-05-09 01:28:20 +0300

    slider issues fiixed

--- src/LogoSlider.jsx
+++ src/LogoSlider.jsx
@@ -6,23 +6,23 @@ const LogosSlider = () => {
     const logos_2 = ['Mountain View.png', 'Ora.png', 'Orascom.png', 'Palm Hills.png', 'People & Places.png', 'Sodic.png', 'taj misr.png', 'Tatweer.png']
     return (
         <div class="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-            <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none"  style={{animation: '20s slide infinite linear',animationPlayState:state}}>
+            <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none" style={{animation: '20s slide infinite linear',animationPlayState:state}}>
                 {
                     [...logos_1, ...logos_2].map(logo => (
 
                         <li>
-                            <img className=" h-16  md:h-[100px]" src={`./logos/${logo}`} alt="Your Image" />
+                            <img className="h-16 md:w-[175px] md:h-[75px]" src={`./logos/${logo}`} alt="Your Image" />
                         </li>
 
                     ))
                 }
             </ul>
-            <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none"   style={{animation: '20s slide infinite linear',animationPlayState: state}}>
+            <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none" style={{animation: '20s slide infinite linear',animationPlayState: state}}>
                 {
                     [...logos_1, ...logos_2].map(logo => (
 
                         <li>
-                            <img className=" h-16  md:h-[100px]" src={`./logos/${logo}`} alt="Your Image" />
+                            <img className="w-16 h-16 md:w-[175px] md:h-[75px]" src={`./logos/${logo}`} alt="Your Image" />
                         </li>
 
                     ))
